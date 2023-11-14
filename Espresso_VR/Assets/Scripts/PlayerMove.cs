ed3fec4b1c8534750ab97a9f14c442a2522d97fb
Evitha-CS <93393181+Evitha-CS@users.noreply.github.com>
2023-11-14 18:46:49 -0300

    Importanción y Configuración de Google Cardboard

---
+++ Espresso_VR/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+   public int playerSpeed;
+   public Transform vrCamera;
+   public float toggleAngle;
+   public bool moveForward;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(vrCamera.eulerAngles.x >= toggleAngle && vrCamera.eulerAngles.x < 90.00f)
+        {
+            moveForward = true;
+        }
+        else
+        {
+            moveForward = false;
+        }
+        if (moveForward)
+        {
+            transform.position = transform.position + Camera.main.transform.forward * playerSpeed * Time.deltaTime;
+        }
+    }
+}
