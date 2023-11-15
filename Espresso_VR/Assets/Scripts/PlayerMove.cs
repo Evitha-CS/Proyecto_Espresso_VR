9bd17876edd4fa661d74f06c845951fdf7066153
Evitha-CS <93393181+Evitha-CS@users.noreply.github.com>
2023-11-15 20:53:22 -0300

    Avance Player
    Se agregó el modelo del jugador, pero falta configurar bien la cámara para que no transpase el modelo al avanzar

--- Espresso_VR/Assets/Scripts/PlayerMove.cs
+++ Espresso_VR/Assets/Scripts/PlayerMove.cs
@@ -5,18 +5,21 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     public int playerSpeed;
+    public float rotationSpeed; // Nueva variable para controlar la velocidad de rotación
     public Transform vrCamera;
     public float toggleAngle;
+    
     public bool moveForward;
 
     private Rigidbody rb;
+    public Transform playerModel; // Referencia al modelo 3D
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
 
-    
+    // Update is called once per frame
     void Update()
     {
         if (vrCamera.eulerAngles.x >= toggleAngle && vrCamera.eulerAngles.x < 90.00f)
@@ -27,13 +30,22 @@ public class PlayerMove : MonoBehaviour
         {
             moveForward = false;
         }
+
+        // Dirección hacia adelante de la cámara
+        Vector3 forward = vrCamera.TransformDirection(Vector3.forward);
+
         if (moveForward)
         {
-            // Se obtiene la dirección hacia adelante de la cámara
-            Vector3 forward = vrCamera.TransformDirection(Vector3.forward);
-
-            // Se aplica el movimiento al Rigidbody usando MovePosition
+            // Aplica el movimiento al Rigidbody usando MovePosition
             rb.MovePosition(rb.position + forward * playerSpeed * Time.deltaTime);
         }
+
+        // Calcula la rotación en el plano Y (vertical) de la cámara
+        float yRotation = vrCamera.eulerAngles.y;
+
+        // Aplica la rotación al objeto hijo (modelo 3D) 
+        playerModel.rotation = Quaternion.Slerp(playerModel.rotation, Quaternion.Euler(0, yRotation, 0), rotationSpeed * Time.deltaTime);
+
     }
+
 }
