a2923d8e951958a6ea639db0c767b39ceb79ef2f
Evitha-CS <93393181+Evitha-CS@users.noreply.github.com>
2023-11-20 16:52:07 -0300

    Mejoras Varias
    Se cambió el tiempo de carga, se eliminó código innecesario de PlayerMove y se cerraron las puertas

--- Espresso_VR/Assets/Scripts/PlayerMove.cs
+++ Espresso_VR/Assets/Scripts/PlayerMove.cs
@@ -4,25 +4,15 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    public int playerSpeed;
-    public float rotationSpeed; // Nueva variable para controlar la velocidad de rotación
-    public Transform vrCamera;
-    public float toggleAngle;
-    
-    public bool moveForward;
-
-    private Rigidbody rb;
-    public Transform playerModel; // Referencia al modelo 3D
-
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-    }
+   public int playerSpeed;
+   public Transform vrCamera;
+   public float toggleAngle;
+   public bool moveForward;
 
     // Update is called once per frame
     void Update()
     {
-        if (vrCamera.eulerAngles.x >= toggleAngle && vrCamera.eulerAngles.x < 90.00f)
+        if(vrCamera.eulerAngles.x >= toggleAngle && vrCamera.eulerAngles.x < 90.00f)
         {
             moveForward = true;
         }
@@ -30,22 +20,9 @@ public class PlayerMove : MonoBehaviour
         {
             moveForward = false;
         }
-
-        // Dirección hacia adelante de la cámara
-        Vector3 forward = vrCamera.TransformDirection(Vector3.forward);
-
         if (moveForward)
         {
-            // Aplica el movimiento al Rigidbody usando MovePosition
-            rb.MovePosition(rb.position + forward * playerSpeed * Time.deltaTime);
+            transform.position = transform.position + Camera.main.transform.forward * playerSpeed * Time.deltaTime;
         }
-
-        // Calcula la rotación en el plano Y (vertical) de la cámara
-        float yRotation = vrCamera.eulerAngles.y;
-
-        // Aplica la rotación al objeto hijo (modelo 3D) 
-        playerModel.rotation = Quaternion.Slerp(playerModel.rotation, Quaternion.Euler(0, yRotation, 0), rotationSpeed * Time.deltaTime);
-
     }
-
 }
