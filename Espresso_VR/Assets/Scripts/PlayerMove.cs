ab1292a4552524e29bac73b4fb3855120f555538
Evitha-CS <93393181+Evitha-CS@users.noreply.github.com>
2023-11-22 11:32:28 -0300

    Aplicación de OclusionCulling en el juego

--- Espresso_VR/Assets/Scripts/PlayerMove.cs
+++ Espresso_VR/Assets/Scripts/PlayerMove.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    public int playerSpeed;
+    public float playerSpeed;
     public Transform vrCamera;
     public float toggleAngle;
     
